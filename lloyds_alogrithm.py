fc0c7f2aa57a06e46652abbe15d14dc7fa7c9fe8
Jared <jaredtponting@gmail.com>
2024-02-28 20:12:46 +0000

    added points out of bounds to add lines leaving screen. Havent figured out how to get area of shapes for those out side of bounds

--- lloyds_alogrithm.py
+++ lloyds_alogrithm.py
@@ -19,34 +19,78 @@ def main():
     RADIUS = 2
 
     def create_random_vectors(max_x, max_y, number):
+        """
+
+        :param max_x: max x coord
+        :param max_y: max y xoord
+        :param number: number of random points
+        :return: np.array of np.array [x y] coords
+        """
         output = []
         for _ in range(number):
             output.append([randint(0, max_x), randint(0, max_y)])
 
+        output.append([-10000, 10000])
+        output.append([-10000, -10000])
+        output.append([10000, -10000])
+        output.append([10000, 10000])
+
         return np.array(output)
 
+    def point_oob(xy):
+        """
+        Determines if point is out of bounds
+        :param xy:
+        :return: True if out of bounds
+        """
+        for coord in xy:
+            if np.abs(coord) > SCREEN_HEIGHT:
+                return True
+
     def calculate_center(regions, region_vertices, point_regions):
-        to_return = {}
+        """
+        Calculates the centre of the regions
+
+        :param regions:
+        :param region_vertices:
+        :param point_regions:
+        :return:
+        """
+        # Need to consider edge of screen
+        to_return = []
         for ind, value in enumerate(point_regions):
             region = regions[value]
-            if -1 in region:
-                continue
-            region_vertex_list = [region_vertices[i] for i in region]
+            is_region_edge = True if -1 in region else False
+            region_vertex_list = [region_vertices[i] for i in region if i != -1]
+            # if any of region vertex list is out of bounds then we need to do some tricky stuff otherwise we do it normal
+            # if any([point_oob(region_points) for region_points in region_vertex_list]):
+            #     center_x =
+            # else:
             center_x = np.average([vert[0] for vert in region_vertex_list])
             center_y = np.average([vert[1] for vert in region_vertex_list])
             center = [center_x, center_y]
-            to_return[ind] = center
+            to_return.append(center)
 
-        return to_return
+        return np.array(to_return)
 
     def average(a, b):
         return (a + b) / 2
 
     def move_towards_center(point_list, centers):
+        """
+        moves point towards centre of region
+
+        :param point_list:
+        :param centers:
+        :return:
+        """
+        # Exclude points that are out of bounds
         to_return = []
         for key in centers.keys():
             value = centers[key]
             point = point_list[key]
+            if point_oob(point):
+                continue
             new_position = [average(point[0], value[0]), average(point[1], value[1])]
             to_return.append(new_position)
 
@@ -58,8 +102,11 @@ def main():
         voron_vertices = voron.vertices
         return voron_regions, voron_vertices, voron.point_region
 
-    points = create_random_vectors(WIDTH, HEIGHT, 5000)
+    points = create_random_vectors(WIDTH, HEIGHT, 100)
     vor_regions, vor_vertices, vor_point_region = voronoi_diagram(points)
+
+    # calculate region centers
+    region_centers = calculate_center(vor_regions, vor_vertices, vor_point_region)
 
     run = True
     while run:
@@ -80,13 +127,10 @@ def main():
             pygame.draw.circle(screen, (255, 0, 0), (point[0], point[1]), radius=RADIUS)
 
         # calculate region centers
-        region_centers = calculate_center(vor_regions, vor_vertices, vor_point_region)
-
-        # calculate move towards centers
-        points = move_towards_center(points, region_centers)
+        # points = calculate_center(vor_regions, vor_vertices, vor_point_region)
 
         # re calculate voronoi stuff
-        vor_regions, vor_vertices, vor_point_region = voronoi_diagram(points)
+        # vor_regions, vor_vertices, vor_point_region = voronoi_diagram(points)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
